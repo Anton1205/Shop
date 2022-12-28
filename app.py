f92e7fb2c3025d9e9a0910463e3bcd794e8f2d82
Anton <antonchik18@icloud.com>
2022-12-28 11:10:21 +0300

    files

--- app.py
+++ app.py
@@ -3,6 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+db = SQLAlchemy()
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer, primary_key=True)
+    isActive = 0
+    text = db.Column(db.Text, nullable=False)
+    amount = db.Column(db.Integer, primary_key=True)
 
 
 @app.route('/')
